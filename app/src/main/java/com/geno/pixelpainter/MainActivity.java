b1808ef82d63989a38de2377e786e3c2d133f384
Y. Z. Chen <754097987@qq.com>
2016-05-04 15:24:01 +0800

    [update] Finish base.

--- app/src/main/java/com/geno/pixelpainter/MainActivity.java
+++ app/src/main/java/com/geno/pixelpainter/MainActivity.java
@@ -4,42 +4,178 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import java.util.Locale;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends Activity
 {
+	public static final int[] COLORLIST = new int[]
+			{
+					0xff000000,
+					0xff008f08,
+					0xff0e09a8,
+					0xff3b34ff,
+					0xff41e157,
+					0xff4ba0ff,
+					0xff861300,
+					0xffa6e5ff,
+					0xffc4ca00,
+					0xffd74000,
+					0xffffae0a,
+					0xffffd2b4,
+					0xffffde9c,
+					0xffffffff,
+			};
+
+	public GridLayout grid;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		GridLayout l = new GridLayout(this);
-		l.setColumnCount(16);
-		setContentView(l);
+		grid = new GridLayout(this);
+		grid.setColumnCount(16);
+		setContentView(grid);
 		for (int i = 0; i < 256; i++)
 		{
 			ImageView v = new ImageView(this);
+			v.setBackgroundColor(Color.BLACK);
 			v.setOnClickListener(new View.OnClickListener()
 			{
 				@Override
 				public void onClick(View v)
 				{
-					ColorPicker2(MainActivity.this, 0, v);
+					ColorPicker(MainActivity.this, v);
 				}
 			});
-			l.addView(v);
+			grid.addView(v, 40, 40);
+		}
+	}
+
+	public static void ColorPicker(final Context context, final View view)
+	{
+		ScrollView s = new ScrollView(context);
+		LinearLayout l = new LinearLayout(context);
+		l.setOrientation(LinearLayout.VERTICAL);
+		s.addView(l);
+		final Dialog d;
+		AlertDialog.Builder ab = new AlertDialog.Builder(context);
+				/*.setAdapter(new BaseAdapter()
+				{
+					@Override
+					public int getCount()
+					{
+						return COLORLIST.length;
+					}
+
+					@Override
+					public Object getItem(int position)
+					{
+						return COLORLIST[position];
+					}
+
+					@Override
+					public long getItemId(int position)
+					{
+						return 0;
+					}
+
+					@Override
+					public View getView(int position, View convertView, ViewGroup parent)
+					{
+						ImageView v = new ImageView(context);
+						v.setBackgroundColor(COLORLIST[position]);
+						return v;
+					}
+				}, new DialogInterface.OnClickListener()
+				{
+					@Override
+					public void onClick(DialogInterface dialog, int which)
+					{
+						view.setBackgroundColor(COLORLIST[which]);
+					}
+				});*/
+		ab.setView(s);
+		d = ab.create();
+		for (final int color : COLORLIST)
+		{
+			ImageView v = new ImageView(context);
+			v.setBackgroundColor(color);
+			l.addView(v, LinearLayout.LayoutParams.MATCH_PARENT, 100);
+			v.setOnClickListener(new View.OnClickListener()
+			{
+				@Override
+				public void onClick(View v)
+				{
+					view.setBackgroundColor(color);
+					d.dismiss();
+				}
+			});
+		}
+		d.show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		menu.add("save");
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		save();
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void save()
+	{
+		String toWrite;
+		int[] colorList = new int[grid.getChildCount()];
+		toWrite = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+				  "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" +
+				  "<svg width=\"160px\" height=\"160px\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n";
+		for (int i = 0; i < grid.getChildCount(); i++)
+		{
+			ImageView v = (ImageView) grid.getChildAt(i);
+			colorList[i] = ((ColorDrawable) v.getBackground().mutate()).getColor();
+			toWrite += "<rect x=\"" + (i % 16) * 10 + "\" y=\"" + (i / 16) * 10 + "\" width=\"10\" height=\"10\" fill=\"#" + Integer.toHexString(colorList[i]).substring(2) + "\"/>";
+		}
+		toWrite += "</svg>";
+		Log.d("Save", toWrite);
+		try
+		{
+			File f = new File(Environment.getExternalStorageDirectory().getPath() + "/a.svg");
+			FileOutputStream fos = new FileOutputStream(f);
+			fos.write(toWrite.getBytes());
+			fos.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
 		}
 	}
 
